24164faa3a5be57fdf7fdb0be0f9245185f6faf5
TooIneptNeedToLearn <moltencore23@gmail.com>
2023-10-30 19:05:23 +0800

    hello

---
+++ app.js
@@ -0,0 +1,95 @@
+let popup1 = document.getElementById("popup1")
+let popup2 = document.getElementById("popup2")
+let popup3 = document.getElementById("popup3")
+let popup4 = document.getElementById("popup4")
+let popup5 = document.getElementById("popup5")
+let popup6 = document.getElementById("popup6")
+let popup7 = document.getElementById("popup7")
+let popup8 = document.getElementById("popup8")
+let popup9 = document.getElementById("popup9")
+let video = document.getElementById("link-video-btn")
+
+/* button1 popup */
+function openpopup1(){
+    popup1.classList.add("openpopup1");
+}
+
+function closepopup1(){
+    popup1.classList.remove("openpopup1");
+}
+
+/* buttonn2 popup */
+function openpopup2(){
+    popup2.classList.add("openpopup2");
+}
+
+function closepopup2(){
+    popup2.classList.remove("openpopup2");
+}
+
+/* button 3 popup */
+function openpopup3(){
+    popup3.classList.add("openpopup3");
+}
+
+function closepopup3(){
+    popup3.classList.remove("openpopup3");
+}
+
+/* button 4 popup */
+function openpopup4(){
+    popup4.classList.add("openpopup4");
+}
+
+function closepopup4(){
+    popup4.classList.remove("openpopup4");
+}
+
+/* button 5 popup */
+function openpopup5(){
+    popup5.classList.add("openpopup5");
+}
+
+function closepopup5(){
+    popup5.classList.remove("openpopup5");
+}
+
+/* button 6 popup */
+function openpopup6(){
+    popup6.classList.add("openpopup6");
+}
+
+function closepopup6(){
+    popup6.classList.remove("openpopup6");
+}
+
+/* button 7 popup */
+function openpopup7(){
+    popup7.classList.add("openpopup7");
+}
+
+function closepopup7(){
+    popup7.classList.remove("openpopup7");
+}
+
+/* button 8 popup */
+function openpopup8(){
+    popup8.classList.add("openpopup8");
+}
+
+function closepopup8(){
+    popup8.classList.remove("openpopup8");
+}
+
+/* button 9 popup */
+function openpopup9(){
+    popup9.classList.add("openpopup9");
+}
+
+function closepopup9(){
+    popup9.classList.remove("openpopup9");
+}
+
+document.getElementById("vidbtn").addEventListener("click", function() {
+    window.location.href = "https://youtu.be/K29HZ_Kl0Z8";
+});
